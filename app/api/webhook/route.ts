7b2c3382e3664c1e2aa8ae1543e97ba6a8b39017
Sudhanshu <110216470+Sudhanshu8257@users.noreply.github.com>
2023-11-23 10:02:01 +0530

    Update route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -61,14 +61,15 @@ export async function POST(req: Request) {
     // Create a new user in your database
     const mongoUser = await createUser({
       clerkId: id,
-      name: `${first_name}${last_name ? `${last_name}` : ""}`,
+      name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
       username: username!,
       email: email_addresses[0].email_address,
       picture: image_url,
     });
 
     return NextResponse.json({ message: "OK", user: mongoUser });
-  } else if (eventType === "user.updated") {
+  }
+  if (eventType === "user.updated") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
@@ -76,7 +77,7 @@ export async function POST(req: Request) {
     const mongoUser = await updateUser({
       clerkId: id,
       updateData: {
-        name: `${first_name}${last_name ? `${last_name}` : ""}`,
+        name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
         username: username!,
         email: email_addresses[0].email_address,
         picture: image_url,
